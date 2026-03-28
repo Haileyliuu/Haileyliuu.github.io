92b130795ab1230758745d7bf747ce73780f4265
Haileyliuu <haileyliuuu@gmail.com>
2026-03-28 01:57:56 -0700

    more progress on portfolio
    images for rest of projects, reorganized files

---
+++ portfolio/script.js
@@ -0,0 +1,21 @@
+
+fetch("../elements/navbar-art74.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar-art74").innerHTML = data;
+
+        const links = document.querySelectorAll(".nav a");
+
+        const currentPage = window.location.pathname.split("/").pop() || "index.html";
+        links.forEach(link => {
+            let linkPage = link.getAttribute("href");
+
+            // Normalize paths (important!)
+            linkPage = linkPage.split("/").pop();
+
+            if (linkPage === currentPage) {
+                link.classList.add("active");
+            }
+        });
+    });
+
