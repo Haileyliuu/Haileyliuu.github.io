9b3a543f331e5b879ac5e0aaf808a4d26341e42b
Haileyliuu <haileyliuuu@gmail.com>
2026-03-28 23:45:34 -0700

    base portfolio art 74 done
    nav bar, images, words complete and working, could still be refined

--- portfolio/script.js
+++ portfolio/script.js
@@ -19,3 +19,23 @@ fetch("../elements/navbar-art74.html")
         });
     });
 
+fetch("/portfolio/elements/navbar-main.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar-main").innerHTML = data;
+
+        const links = document.querySelectorAll(".nav a");
+
+        const currentPage = window.location.pathname.split("/").pop() || "index.html";
+        links.forEach(link => {
+            let linkPage = link.getAttribute("href");
+
+            // Normalize paths (important!)
+            linkPage = linkPage.split("/").pop();
+
+            if (linkPage === currentPage) {
+                link.classList.add("active");
+            }
+        });
+    });
+
